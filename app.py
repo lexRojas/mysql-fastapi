89869aa468525aabbb193766329db179f0e05185
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2024-04-05 17:04:25 -0600

    add CORS

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
 from routes.indices import indice
 from routes.actos   import actos
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+origins = ['http://localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+
+
 
 
 
