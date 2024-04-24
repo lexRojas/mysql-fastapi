6d4e04d7501985cd29fac60b9304b0f41f3ce6ed
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2024-04-24 13:47:32 -0600

    password

--- db/db.py
+++ db/db.py
@@ -5,7 +5,7 @@ conn= mysql.connector.connect(
 
   host="erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user="k1hpp1oicmrsuqfh",
-  password="tsaxu6wsnhor1d2u",
+  password="iei93plst88ydzp5",
   database="vmy3gj1q7kthw5c8"
 
 )
