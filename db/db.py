7aff24d8f751456d702b80017fe9b0aa3d788687
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2024-04-24 20:38:16 -0600

    Update db.py

--- db/db.py
+++ db/db.py
@@ -5,7 +5,7 @@ conn= mysql.connector.connect(
 
   host="dyud5fa2qycz1o3v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user="x24e41tovl9m7b66",
-  password="iei93plst88ydzp5",
+  password="nqee0rp7hcuarhcp",
   database="dkd23x0ptykxgrbd"
 
 )
