509a5382d5dc54a53f9b1c254e13503a87d5061b
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2024-04-25 15:27:30 -0600

    cec

--- db/db.py
+++ db/db.py
@@ -3,10 +3,10 @@ import mysql.connector
 
 conn= mysql.connector.connect( 
 
-  host="dyud5fa2qycz1o3v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user="x24e41tovl9m7b66",
-  password="nqee0rp7hcuarhcp",
-  database="dkd23x0ptykxgrbd"
+  host="localhost",
+  user="fastapi",
+  password="password",
+  database="notario"
 
 )
 
