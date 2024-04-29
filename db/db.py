835cf8e256011d4a88094d4b64285b4892350396
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2024-04-28 19:05:51 -0600

    xxxxx

--- db/db.py
+++ db/db.py
@@ -3,9 +3,9 @@ import mysql.connector
 
 conn= mysql.connector.connect( 
 
-  host="localhost",
-  user="fastapi",
-  password="password",
+  host="18.222.180.241",
+  user="awsuser",
+  password="G30rg1n@",
   database="notario"
 
 )
