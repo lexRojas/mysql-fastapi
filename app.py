31b432d4e8a0444635f47a0fd33564616e906722
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2024-05-04 22:36:13 -0600

    dcvdv

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 
-origins = ['http://localhost:3000', 'https://lexnotario.netlify.app']
+origins = ['http://localhost:3000', 'https://lexnotario.netlify.app/*']
 
 app.add_middleware(
     CORSMiddleware,
