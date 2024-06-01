f199698499dfb3e3719628c9caf1733ebded39e4
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2024-05-31 18:52:30 -0600

    IP SERVER

--- db/db.py
+++ db/db.py
@@ -3,7 +3,7 @@ import mysql.connector
 # datoss de bases de datos 
 conn= mysql.connector.connect( 
 
-  host="18.222.180.241",
+  host="18.221.154.102",
   user="rrojas",
   password="G30rg1n@",
   database="notario"
