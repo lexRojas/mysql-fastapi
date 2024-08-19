c8200048f21698e088e43f41cc67ed026307cf31
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2024-08-19 11:37:00 -0600

    no cache

--- routes/indices.py
+++ routes/indices.py
@@ -9,7 +9,7 @@ indice = APIRouter()
 @indice.get("/indice")
 async def getIndice():
     with conn.cursor(dictionary=True) as mycursor:
-        mycursor.execute("select * from valores_usuales")
+        mycursor.execute("select SQL_NO_CACHE * from valores_usuales")
         rst = mycursor.fetchall()
     
     # conn.close()
@@ -20,7 +20,7 @@ async def getIndice():
 async def getIndiceByDates(fecha_inicio:date=None, fecha_final:date=None):
     with conn.cursor(dictionary=True) as mycursor:
 
-        sql = "select * from valores_usuales where fecha between %s and %s"
+        sql = "select SQL_NO_CACHE * from valores_usuales where fecha between %s and %s"
         params= [
                 fecha_inicio, 
                 fecha_final
