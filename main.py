6549cc4d0a1e148f7bd27722716fa1302af66d30
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2024-09-24 09:20:13 -0600

    api nuevo

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import uvicorn
 
 
 if __name__== "__main__":
-    # uvicorn.run("app:app", reload=True,host='0.0.0.0', port=8000)
-    uvicorn.run("api:app", reload=True, port=8000)
+    uvicorn.run("api:app", reload=True,host='0.0.0.0', port=8000)
+    # uvicorn.run("api:app", reload=True, port=8000)
 
 # from sqlalchemy.orm import Session
 
