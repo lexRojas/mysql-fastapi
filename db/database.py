0e8298b2d86d88a8f92a9375ef7f478f190efb9f
lexrojas <legalcr.rojas@gmail.com>
2024-11-27 09:56:52 -0600

    cambio db

--- db/database.py
+++ db/database.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from databases import Database
 
 # URL de la base de datos MySQL
-DATABASE_URL = "mysql+asyncmy://rrojas:G30rg1n@@18.221.154.102:3306/notario"
+# DATABASE_URL = "mysql+asyncmy://rrojas:G30rg1n@@18.221.154.102:3306/notario"
+DATABASE_URL="mysql://admin:G30rg1n4@mysql-187830-0.cloudclusters.net:10072/burbujas"
 
 # Crear el motor asíncrono de SQLAlchemy
 engine = create_async_engine(DATABASE_URL, echo=True)
